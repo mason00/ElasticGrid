bc298621cb233b55830fb6fe675312bdfcb290ba
mason00 <masondev@gmail.com>
2016-08-03 23:46:53 +1000

    jQueryPlugin

---
+++ ElasticGrid/Scripts/Plugin.js
@@ -0,0 +1,24 @@
+﻿$.fn.makeLink = function (options) {
+    var settings = $.extend({
+        height: '800',
+        width: '600',
+        top: '100',
+        left: '100',
+        scrollbars: 'yes',
+    }, options);
+
+    this.filter('a').each(function (index) {
+        var link = $(this);
+        var href = link.attr('href');
+        var name = link.attr('data-window-group');
+        link.click(function () {
+            window.open(href, name, 'height=' + options.height + 
+                                    ',width=' + options.width + 
+                                    ',top=' + options.top +
+                                    ',left=' + options.left +
+                                    ',scrollbars=' + options.scrollbars);
+            return false;
+        });
+    });
+    return this;
+}
---
+++ ElasticGrid/Scripts/PluginSpec.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="../Scripts/jasmine.js"/>
+/// <reference path="./jquery-1.10.2.min.js" />
+/// <reference path="./Plugin.js" />
+
+describe("string startsWith", function () {
+    it("test plugin", function () {
+        $('body').append($("<a data-window-group='a' href='test.html'>test</a><br/>" +
+          "<a data-window-group='a' href='test1.html'>test</a><br/>" +
+          "<a data-window-group='b' href='test2.html'>test</a><br/>" +
+          "<a data-window-group='b' href='test3.html'>test</a><br/>").makeLink({
+              height: 500,
+              width: 500,
+              top: 100,
+              left: 100,
+              scrollbars: 'no'
+          }));
+    });
+}); 
